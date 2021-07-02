7c8e141749f82db83c4cde5557b750eedde76b1b
akofisher <akolekveishvili@gmail.com>
2021-07-02 14:11:45 +0400

    NEWNEW

--- src/App.js
+++ src/App.js
@@ -1,121 +1,51 @@
 import React from "react";
 import "./App.scss";
-import { Formik, useField, Form } from "formik";
-import * as Yup from "yup";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Box } from "@material-ui/core";
 
-const CustomTextInput = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-
-  return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
-
-const CustomCheckBox = ({ children, ...props }) => {
-  const [field, meta] = useField(props, "checkbox");
-
-  return (
-    <>
-      <label className="checkbox">
-        <input type="checkbox" {...field} {...props} />
-        {children}
-      </label>
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
-
-const CustomSelect = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-
-  return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <select {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    width: "100%",
+    margin: "0px",
+  },
+  box: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    background: theme.palette.success.light,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <Formik
-        initialValues={{
-          name: "",
-          email: "",
-          acceptedTerms: false,
-          gender: "",
-        }}
-        validationSchema={Yup.object({
-          name: Yup.string()
-            .min(5, "უნდა შეიცავდეს მინიმუმ 5 სიმბოლოს")
-            .max(15, "უნდა შეიცავდეს მაქსიმუმ 15 სიმბოლოს")
-            .required("აუცილებელია"),
-          email: Yup.string()
-            .email("არასწორი მეილის მისამართი")
-            .required("აუცილებელია"),
-          acceptedTerms: Yup.boolean()
-            .required("აუცილებელია")
-            .oneOf([true], "ამას აუცილებელია დაეთანხმო, ისე არ გამოვა"),
-          gender: Yup.string()
-            .oneOf(
-              ["მამაკაცი", "დედაკაცი", "სხვა", "უფრო სხვა", "აბა მე რავი"],
-              "ნუ იქნები ჰომოფობი"
-            )
-            .required("აუცილებელია"),
-        })}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            resetForm();
-            setSubmitting(false);
-          }, 2000);
-        }}
-      >
-        {(props) => (
-          <Form>
-            <h1>რეგისტრაცია</h1>
-            <CustomTextInput
-              label="სახელი"
-              name="name"
-              type="text"
-              placeholder="Rame Name"
-            />
-            <CustomTextInput
-              label="ელ-ფოსტა"
-              name="email"
-              type="email"
-              placeholder="Rame@gmail.com"
-            />
-            <CustomSelect label="გენდერი" name="gender">
-              <option value="">აირჩიეთ გენდერი</option>
-              <option value="მამაკაცი">მამაკაცი</option>
-              <option value="დედაკაცი">დედაკაცი</option>
-              <option value="სხვა">სხვა</option>
-              <option value="უფრო სხვა">უფრო სხვა</option>
-              <option value="აბა მე რავი">აბა მე რავი</option>
-            </CustomSelect>
-            <CustomCheckBox name="acceptedTerms">
-              მე თანახმა ვარ დაუწიონ მზეს 15 გრადუსით
-            </CustomCheckBox>
-            <button type="submit">
-              {props.isSubmitting ? "იტვირთება..." : "Submit"}{" "}
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+    <Grid container spacing={5} className={classes.grid}>
+      <Grid item xs={12}>
+        <Box className={classes.box}>1</Box>
+      </Grid>
+      <Grid item xs={12}>
+        <Box className={classes.box}>2</Box>
+      </Grid>
+      <Grid item xs={3}>
+        <Box className={classes.box}>3</Box>
+      </Grid>
+      <Grid item xs={3}>
+        <Box className={classes.box}>4</Box>
+      </Grid>
+      <Grid item xs={6}>
+        <Box className={classes.box}>5</Box>
+      </Grid>
+      <Grid item xs={4}>
+        <Box className={classes.box}>6</Box>
+      </Grid>
+      <Grid item xs={4}>
+        <Box className={classes.box}>7</Box>
+      </Grid>
+      <Grid item xs={4}>
+        <Box className={classes.box}>8</Box>
+      </Grid>
+    </Grid>
   );
 }
 
